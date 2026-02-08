b8b41743822fca9b65af2d30e0dfaa4625aef57b
RakeshMahendran <rakeshmahendran99@gmail.com>
2026-02-09 01:04:13 +0530

    added docker

---
+++ server/install_chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Install Chrome for Render deployment
+
+echo "Installing Chrome dependencies..."
+
+# Install Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+echo "Chrome installed successfully!"
